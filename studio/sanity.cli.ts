23808abdf1a9b7b1572c36b03dcf79b1b7d4f837
Torleif Halseth <torleif.halseth@spv.no>
2026-06-03 20:52:55 +0200

    fix: correct config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -16,7 +16,7 @@ export default defineCliConfig({
     dataset,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/studio/environment-variables to learn more about using environment variables for local & production.
-  autoUpdates: true,
+  deployment: {autoUpdates: true},
   typegen: {
     path: './src/**/*.{ts,tsx,js,jsx}',
     schema: '../sanity.schema.json',
